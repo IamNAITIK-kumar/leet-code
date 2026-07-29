8352ffc1fb4c6ba962915ef22bcb905121d9745c
IamNAITIK-kumar <naitik282808@gmail.com>
2026-07-29 22:06:19 +0530

    Time: 24 ms (9.56%), Space: 12.2 MB (89.89%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        s = str(x)
+        
+        for i in range(len(s) // 2):
+            if s[i] != s[len(s) - 1 - i]:
+                return False
+
+        return True
