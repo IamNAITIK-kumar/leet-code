b1dde75d5ae6fbbbe699d21c68d52858537fa035
IamNAITIK-kumar <naitik282808@gmail.com>
2026-08-07 13:17:36 +0530

    Time: 0 ms (100%), Space: 12.3 MB (90.26%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def toLowerCase(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        len(s) > 0
+        len(s)<= 100
+
+        return s.lower()
